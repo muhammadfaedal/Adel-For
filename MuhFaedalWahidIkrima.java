1216ef04851a9d0eef5d0eb11969038da5a1ed88
muhammadfaedal <39688768+muhammadfaedal@users.noreply.github.com>
2018-05-28 13:50:55 +0800

    Add files via upload

---
+++ MuhFaedalWahidIkrima.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muhfaedalwahidikrima;
+
+/**
+ *
+ * @author Atri
+ */
+public class MuhFaedalWahidIkrima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic hereh
+        for(int i=1;i<11;i++)
+            System.out.println( i + " * 8 = " + (i*8));
+    }
+}
